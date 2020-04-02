e7226afb8231cbcb72a7d2d1eb397b5e6f3ada8d
Avi tiwari <avithegr807@gmail.com>
2020-04-02 15:16:44 +0545

    Updates

---
+++ precache-manifest.86c125114f7a73b54f17e97fc84c60c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2997f6d3c4f1127a62ab7cf799af323",
+    "url": "/todos-react/public/index.html/index.html"
+  },
+  {
+    "revision": "bb331b65525f23024f46",
+    "url": "/todos-react/public/index.html/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "69b2631181ced80af078",
+    "url": "/todos-react/public/index.html/static/css/main.eba195d6.chunk.css"
+  },
+  {
+    "revision": "bb331b65525f23024f46",
+    "url": "/todos-react/public/index.html/static/js/2.a5ecfcb0.chunk.js"
+  },
+  {
+    "revision": "1af7522b869d484878e92bdce4774652",
+    "url": "/todos-react/public/index.html/static/js/2.a5ecfcb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b2631181ced80af078",
+    "url": "/todos-react/public/index.html/static/js/main.42aed7aa.chunk.js"
+  },
+  {
+    "revision": "896ddd10d7277da03dfb",
+    "url": "/todos-react/public/index.html/static/js/runtime-main.cae98c16.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todos-react/public/precache-manifest.c4589d240adaafd843acb5ae652ebb2f.js"
+  "/todos-react/public/index.html/precache-manifest.86c125114f7a73b54f17e97fc84c60c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todos-react/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todos-react/public/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
