d396819bb5e688cd9bfcd356d954a08601b1cb2c
Avi tiwari <avithegr807@gmail.com>
2020-04-02 16:58:25 +0545

    Updates

---
+++ precache-manifest.ee27e5396b1dd919f8e00f53aab805a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9842907ae7d9b1d499a66091a9476c14",
+    "url": "/todos-react/index.html"
+  },
+  {
+    "revision": "bb331b65525f23024f46",
+    "url": "/todos-react/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "03bc1ca2147d066f4620",
+    "url": "/todos-react/static/css/main.eba195d6.chunk.css"
+  },
+  {
+    "revision": "bb331b65525f23024f46",
+    "url": "/todos-react/static/js/2.a5ecfcb0.chunk.js"
+  },
+  {
+    "revision": "1af7522b869d484878e92bdce4774652",
+    "url": "/todos-react/static/js/2.a5ecfcb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bc1ca2147d066f4620",
+    "url": "/todos-react/static/js/main.3ac02902.chunk.js"
+  },
+  {
+    "revision": "13a5c6cdfcc7ede27130",
+    "url": "/todos-react/static/js/runtime-main.d7d9babc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todos-react/public/index.html/precache-manifest.86c125114f7a73b54f17e97fc84c60c9.js"
+  "/todos-react/precache-manifest.ee27e5396b1dd919f8e00f53aab805a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todos-react/public/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todos-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
